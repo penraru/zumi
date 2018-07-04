1ee59c9fdb8c6581c0f724d1a20f16508699a9b7
firestoneleila <leilafirestone@gmail.com>
2018-07-04 01:58:49 +0000

    Starting to work on perfect turn with accelerometer

---
+++ sample/gyroTurn.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+import smbus
+import math
+import time
+import os
+import sys
+sys.path.insert(0,'/home/pi/Desktop/Linky/src')
+import Linky_SerialCom as ropi
+ 
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+# Initialize constants
+GYRO_SAMPLERATE = 100
+ 
+# Initialize variables
+millis = int(round(time.time()*1000))
+angle = 0.0
+angleNew = 0.0
+
+
+bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
+address = 0x68       # via i2cdetect
+ 
+
+def read_byte(reg):
+	return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+	h = bus.read_byte_data(address, reg)
+	l = bus.read_byte_data(address, reg+1)
+	value = (h << 8) + l
+	return value
+ 
+def read_word_2c(reg):
+	val = read_word(reg)
+	if (val >= 0x8000):
+		return -((65535 - val) + 1)
+	else:
+		return val
+	    
+def get_orientation(millis,angle,angleNew):
+    
+    if int(round(time.time()*1000)) - millis >= GYRO_SAMPLERATE:
+        gyroscope_zout = read_word_2c(0x47)
+        gyroscope_zout_scaled = gyroscope_zout / 131
+        if abs(gyroscope_zout_scaled) > 5:
+            angleNew = gyroscope_zout_scaled * GYRO_SAMPLERATE/1000 *0.98
+            if angleNew > 0:
+                angleNew *= 90/80.0
+            else:
+                angleNew *= 90/100.0
+            angle+=angleNew
+        millis = int(round(time.time()*1000))
+    return angle
+
+
+bus.write_byte_data(address, power_mgmt_1, 0)
+ 
+gyroscope_zout = read_word_2c(0x47)
+ 
+gyroscope_zout_scaled = gyroscope_zout / 131
+
+
+print "I am getting ready to turn"
+time.sleep(2)#Need sleep before turning for some weird reason?
+ropi.left()
+time.sleep(1)#Turn for one second
+ropi.stop()
+print "I have turned"
+
+#while 1:
+    #angle = get_orientation(millis,angle,angleNew)
+    #print angle
+    
+
+

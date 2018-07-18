23d87e665436dee5bf2b05e55859efd877bb86e5
joonspoon <joonspoon@joonspoon.com>
2018-07-18 21:13:52 +0000

    Created engine function doc

---
+++ lib/Engine.py
@@ -0,0 +1,26 @@
+#All of the functions necessary to move Linky
+import time
+import sys
+sys.path.insert(0,'/home/pi/Desktop/Linky/src')
+import Linky_SerialCom as ropi
+
+speed = 30
+
+def go_straight():
+    ropi.setMotor(speed, speed)
+    print(speed)
+
+def turn_left():
+    ropi.setMotor(speed, -speed)
+
+def turn_right():
+    ropi.setMotor(-speed, speed)
+
+def stop():
+    ropi.setMotor(0,0)
+
+def set_speed(s):
+    speed = s
+
+while 1:
+    go_straight()

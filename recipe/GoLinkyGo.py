fa381acf742221bd0f292845ddaca3e7416967eb
June Clarke <joonspoon@joonspoon.com>
2018-07-19 15:18:54 -0700

    Put GoLinkyGo into recipe format.

--- recipe/GoLinkyGo.py
+++ recipe/GoLinkyGo.py
@@ -3,6 +3,9 @@ import time
 sys.path.insert(0,'/home/pi/Desktop/Linky/lib')
 import Engine as engine
 
-engine.go_forward()
-time.sleep(3)
-engine.stop()
+# To make Linky move, you should use her Engine.
+# See her available functions at Linky/lib/Engine.py
+# Try to make her go forward, stop and go backwards.
+
+# WILLIAM: TO BE REMOVED ONCE TESTING IS COMPLETE 
+# engine.go_forward()

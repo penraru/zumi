ca39c7de1de826ac77032ff22ebf62dc85d9007a
joonspoon <joonspoon@joonspoon.com>
2018-07-23 23:53:42 +0000

    Added sleeps to get infrared reads to work better.

--- lib/Infrared.py
+++ lib/Infrared.py
@@ -1,15 +1,19 @@
 import sys
 sys.path.insert(0,'/home/pi/Desktop/Linky/src')
 import Linky_SerialCom as linky
+import time
 
 def get_front_distance():
+    time.sleep(.2)
     topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
     return topMiddle
 
 def get_left_distance():
+    time.sleep(.2)
     topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
     return topLeft
 
 def get_right_distance():
+    time.sleep(.2)
     topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
     return topRight

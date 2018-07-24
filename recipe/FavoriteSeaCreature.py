4b60bd9f5abdc46ddba0943d15f29d88d50cc24f
joonspoon <joonspoon@joonspoon.com>
2018-07-24 14:43:37 -0700

    Added recipe to teach image flipping using openCV online documentation.

---
+++ recipe/FavoriteSeaCreature.py
@@ -0,0 +1,7 @@
+import cv2
+
+# 1. Display the Axolotl image (images/axolotl.jpg), or choose your own favorite sea creature to show
+
+
+# 2. Use the Internet to figure out how to flip the Axolotl. Use "openCV" and "Python" in your search.
+

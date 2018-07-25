13c950a2d43220cf6b55bffd881b5c06ae3b0bfa
joonspoon <joonspoon@joonspoon.com>
2018-07-24 17:13:06 -0700

    Added recipe to teach how to break out of an openCV loop.

--- recipe/Mirror.py
+++ recipe/Mirror.py
@@ -1,8 +1,11 @@
 # CHALLENGE: Show frames from your web cam until a key is pressed
+
+import cv2
+
 while True:
-    try:
-        print "hi"
-    except KeyboardInterrupt:
+    print "hi"
+    cv2.namedWindow('Press Q to Quit', cv2.WINDOW_NORMAL)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 print "done"

abe54636004dba42075dea23093cb0cb5f01b71c
michaelfwitt <michaelfwitt40@gmail.com>
2018-08-07 22:48:40 +0000

    All motor control commands send strings, not bytes

--- src/Linky_SerialCom.py
+++ src/Linky_SerialCom.py
@@ -18,16 +18,16 @@ def readSensors():
     #SBottomLeft,SBottomRight
     #Battery Level
     #Mic level
-    ser.write("L")
+    ser.write(b'L\n')
     read_serial = ser.readline()
-    topLeft,topMiddle,topRight,bottomLeft,bottomRight,batterylvl, miclvl = read_serial.split(",")
-    return int(topLeft),int(topMiddle),int(topRight),int(bottomLeft),int(bottomRight),float(batterylvl), int(miclvl)
+    topLeft,topMiddle,topRight,bottomLeft,bottomRight,batterylvl,miclvl = read_serial.split(",")
+    return int(topLeft),int(topMiddle),int(topRight),int(bottomLeft),int(bottomRight),float(batterylvl),int(miclvl)
 
 #returns a tuple with the data as integers
 def requestData():
     #ON linky only speed is currently requested
 
-    ser.write("Z")#REQUEST INFO
+    ser.write(b'Z\n')#REQUEST INFO
 
     read_serial = ser.readline()
     #read the Serial line the rokit should be sending a message
@@ -39,25 +39,25 @@ def requestData():
     return int(servo1Angle),int(servo2Angle),int(servoStep),int(speed),int(a19),int(a20),int(a21)    
 
 def forward():
-    ser.write('M')
+    ser.write(b'M\n')
 
 def backward():
-    ser.write('N')
+    ser.write(b'N\n')
 
 def left():
-    ser.write('O')
+    ser.write(b'O\n')
 
 def right():
-    ser.write('P')
+    ser.write(b'P\n')
     
 def stop():
-    ser.write('Q')
+    ser.write(b'Q\n')
 
 def speedDecrease():
-    ser.write('V')
+    ser.write(b'V\n')
 
 def speedIncrease():
-    ser.write('W')
+    ser.write(b'W\n')
 
 def speed(speedPercentage):
     
@@ -74,88 +74,26 @@ def speed(speedPercentage):
         speedIncrease()
     
 def setSpeed(i):
-    if i < 0:
-        i = 0
-    if i > 100:
-        i = 100
-    j = i%10
-    setModifier((i-j)/10)
-    ser.write('l')
-    setModifier(j)
-    ser.write('m')
-
-def setModifier(i):
-    if i == 0:
-        ser.write(b'a')
-    elif i == 1:
-        ser.write(b'b')
-    elif i == 2:
-        ser.write(b'c')
-    elif i == 3:
-        ser.write(b'd')
-    elif i == 4:
-        ser.write(b'e')
-    elif i == 5:
-        ser.write(b'f')
-    elif i == 6:
-        ser.write(b'g')
-    elif i == 7:
-        ser.write(b'h')
-    elif i == 8:
-        ser.write(b'i')
-    elif i == 9:
-        ser.write(b'j')
-    else:
-        ser.write(b'k')
-
-def flush():
-    ser.flushInput()
-
-def requestObstacle():
-    a = 0
-    if ser.inWaiting():
-        a = 1
-    return a
-
-def followLine():
-    ser.write('K')
-
-def setTurnBias(a):
-    if a == 0:
-        setModifier(0)
-    else:
-        setModifier(1)
-    ser.write('R')
+    ser.write(b'M\n')
 
 def setMotor(i, j):
     """Sets the individual speed of each motor.
     Args: 2 integers, from -100 to 100, for the left and right motors, respectively.
     """
-    reverse1 = 0
-    reverse2 = 0
-    if i < -100:
-        i = -100
-    if i < 0:
-        i *= -1
-        reverse1 = 1
-    if i > 100:
-        i = 100
-    if j < -100:
-        j = -100
-    if j < 0:
-        j *= -1
-        reverse2 = 2
-    if j > 100:
-        j = 100
-    k = i % 10
-    setModifier((i - k) // 10)
-    ser.write(b'q')
-    setModifier(k)
-    ser.write(b'r')
-    k = j % 10
-    setModifier((j - k) // 10)
-    ser.write(b's')
-    setModifier(k)
-    ser.write(b't')
-    setModifier(reverse1 + reverse2)
-    ser.write(b'u')
+    if i<0:
+        i*=-1
+        if j<0:
+            a = 'C'
+            j*=-1
+        else:
+            a = 'B'
+    else:
+        if j<0:
+            a = 'A'
+            j*=-1
+        else:
+            a = '@'
+    a = a + chr(i)
+    a = a + chr(j)
+    a = a + '\n'
+    ser.write(a)

26af8a38a4c1af69a324ef5fc1a2e7704aa1b6c2
joonspoon <joonspoon@joonspoon.com>
2018-09-11 18:41:39 -0700

    Added functions for reading the bottom sensors.

--- lib/Infrared.py
+++ lib/Infrared.py
@@ -4,16 +4,37 @@ import Linky_SerialCom as linky
 import time
 
 def get_front_distance():
-    time.sleep(.2)
+   # time.sleep(.2)
     topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
     return topMiddle
 
 def get_left_distance():
-    time.sleep(.2)
+   # time.sleep(.2)
     topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
     return topLeft
 
 def get_right_distance():
-    time.sleep(.2)
+   # time.sleep(.2)
     topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
     return topRight
+
+def get_bottom_left():
+   # time.sleep(.2)
+    topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
+    return bottomLeft
+
+def get_bottom_right():
+   # time.sleep(.2)
+    topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
+    return bottomRight
+
+def get_bottom_left_color():
+    topLeft, topMiddle, topRight, bottomLeft, bottomRight, batterylvl, miclvl = linky.readSensors()
+    if bottomLeft > 960 and bottomLeft < 1005:
+        return "black"
+    elif bottomLeft > 840 and bottomLeft < 895:
+        return "white"
+    
+
+
+

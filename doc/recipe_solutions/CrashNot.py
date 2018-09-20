d051428704282c2a04e37d9a2967458b5d4ef668
Yealynn Kim <kylynn315@naver.com>
2018-09-20 13:53:02 -0700

    update CrashNot

--- doc/recipe_solutions/CrashNot.py
+++ doc/recipe_solutions/CrashNot.py
@@ -1,22 +1,31 @@
 import sys
-sys.path.insert(0,'/home/pi/Desktop/Linky/lib')
+sys.path.insert(0,'/home/pi/zumi/lib')
+
 import Engine as engine
 import Infrared as infrared
 import time
+import IPython.display
 
 # Use time.sleep(numberOfSeconds) to sleep Linky for 2 seconds before reading the IR sensors
 time.sleep(2)
-
 # Use the Engine to drive forward
+engine.set_speed(15)
 engine.go_forward()
 
 # In a loop, use the Infrared sensors to check if a wall is close
 # Avoid the wall by turning in the opposite direction
+while 1:
+    front_distance = infrared.get_front_distance()
+    left_distance = infrared.get_left_distance()
+    right_distance = infrared.get_right_distance()
 
-while (1):
-    leftDistance = infrared.get_left_distance()
-    rightDistance = infrared.get_right_distance()
-    if leftDistance < 200:
+    print (left_distance + " " + front_distance + " " + right_distance)
+    if front_distance < 650 or left_distance < 600 or right_distance < 600 :
+        print ("I might crash!")            
         engine.turn_right()
-    elif rightDistance < 200:
-        engine.turn_left()
+        time.sleep(1.1)
+        engine.stop()
+        time.sleep(0.3)
+        engine.go_forward()
+    
+    IPython.display.clear_output(wait=True) 

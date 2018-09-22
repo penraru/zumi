3e01e6aec0c06ac7beda7d08cc586b09c29a411f
joonspoon <joonspoon@joonspoon.com>
2018-09-21 18:33:01 -0700

    Tidied up the remote control a bit.

--- sample/ArrowKeyRemote.py
+++ sample/ArrowKeyRemote.py
@@ -1,26 +1,6 @@
 import sys
 sys.path.insert(0,'/home/pi/zumi/lib')
-
 import Engine as engine
-import Infrared as infrared
-import time
-
-# while(True):
-# #if distance is more than 10 cm. go back. Ig there is an obstacle stop
-# #        if (ultrasonicSensorBack.get_distance() > 10):
-# #                vikingbotMotors.goBack()
-
-#     cmd = input("Enter the command ")
-#     print(cmd)
-#     if cmd == 'w':
-#         engine.go_forward()
-#     elif cmd == 'a':
-#         engine.turn_left()
-#     elif cmd == 's':
-#         engine.turn_right()
-#     else: 
-#         engine.stop()
-
 import curses
 
 # get the curses screen window

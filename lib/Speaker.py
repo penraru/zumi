a31dd920ab6104318046d174f1ee1f7835e26354
michaelfwitt <39968538+michaelfwitt@users.noreply.github.com>
2018-10-05 15:48:34 -0700

    Create Speaker.py

---
+++ lib/Speaker.py
@@ -0,0 +1,11 @@
+#Import this file to use all of Zumi's sound functions
+import time
+import sys
+sys.path.insert(0,'/home/pi/zumi/src')
+import Linky_SerialCom as zumi
+
+def play_sad_sound():
+    zumi.beepSad()
+
+def play_happy_sound():
+    zumi.beepHappy()
